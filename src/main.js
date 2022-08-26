21416e07be8c4c7919c766f3eecd94589ec7b215
Ionut Frisan <64963028+Ionut-Frisan@users.noreply.github.com>
2022-08-26 23:27:51 +0300

    updated axios base url for production

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ import ToastService from 'primevue/toastservice';
 
 import router from "./router/index.js";
 //axios.defaults.baseURL = 'https://home-tasks-2.herokuapp.com'
-axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.baseURL = 'https://git.heroku.com/home-tasks-2.git'
 
 
 const app = createApp(App);
